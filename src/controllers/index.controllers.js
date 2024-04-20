b06a4e7deec0759b336f114c7644a412e61a7c7e
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-04-20 12:05:16 -0500

    Portafoleos en el menu principal, Envio de datos del formulario, ambas actividades terminadas

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,5 +1,8 @@
-const renderIndex = (req, res) => {
-  res.render("index");
+const Portfolio = require("../models/Portafolio");
+
+const renderIndex = async (req, res) => {
+  const portfolios = await Portfolio.find().lean();
+  res.render("index", { portfolios });
 };
 const renderAbout = (req, res) => {
   res.render("login");
